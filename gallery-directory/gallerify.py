54f4859084c13262948b90136ceb9810d0771b71
oldflat <smJU.20tackom$eat>
2023-06-10 05:13:08 -0400

    some initial work

---
+++ gallery-directory/gallerify.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+path = 'C:\github_projects'
+
+
+def get_subdirectory_list():
+    pass
+
+
+def main():
+    subdirectory_list = get_subdirectory_list()
+
+    for s in subdirectory_list:
+        print(s)
+
+
+
+if __name__ == "__main__":
+    main()
